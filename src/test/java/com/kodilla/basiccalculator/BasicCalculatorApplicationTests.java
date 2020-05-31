cc9a2dd161e990d9ca94ba29da50a7a7f1b1ddd9
tomaszRuta <tomasz.ruta@o2.pl>
2020-05-31 09:47:41 +0200

    Initialize repository

---
+++ src/test/java/com/kodilla/basiccalculator/BasicCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.basiccalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
